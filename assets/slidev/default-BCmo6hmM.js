04b48907c4bcff308a5521511d24fd9dcb5ce678
kravetsone <57632712+kravetsone@users.noreply.github.com>
2024-04-19 16:57:15 +0000

    Deploying to gh-pages from @ kravetsone/slidev-theme-elysia@cd70b36b1099fbf77c1e4af62114fc6b6b5e17ee 🚀

--- assets/slidev/default-BCmo6hmM.js
+++
@@ -1,1 +0,0 @@
-import{o,b as s,f as t}from"../modules/vue-CVPr9In-.js";import{b as l}from"../index-CFt5Nv8j.js";const r={},a={class:"slidev-layout default"};function n(e,c){return o(),s("div",a,[t(e.$slots,"default")])}const _=l(r,[["render",n],["__file","/home/runner/work/slidev-theme-elysia/slidev-theme-elysia/node_modules/@slidev/client/layouts/default.vue"]]);export{_ as I};
